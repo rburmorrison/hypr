a5e9751b311d1ef39d73f7183f08b00dc987ac4e
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2019-11-16 20:37:41 -0500

    Resolve issue with NewLink

--- html/element.go
+++ html/element.go
@@ -9,14 +9,14 @@ type Element interface {
 	Write(w io.Writer)
 	AddAttr(k, v string)
 	AddChild(Element)
-	}
+}
 
 // NewLink creates a new Element preformatted
 // as an anchor and returns a reference to it.
 func NewLink(link, text string) Element {
 	a := NewTagElement("a")
-	a.AddElement(NewTextElement(text))
+	a.AddChild(NewTextElement(text))
 	a.AddAttr("href", link)
 
-	return &a
+	return a
 }
